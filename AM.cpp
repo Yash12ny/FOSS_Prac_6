7dac6c2b2d68eeadeb880eeb54f072bf0a150666
Yash12ny <31369762+Yash12ny@users.noreply.github.com>
2017-08-27 19:18:38 +0530

    Update AM.cpp

--- AM.cpp
+++ AM.cpp
@@ -10,24 +10,26 @@ int main()
 	int i,sum;
 	sum=0;
 	cout<<" \n Calculating the arithmetic mean of a given data";
+	
 	cout<<" \n Enter the number of elements : ";
 	cin>>arraysize;
-	for(i=0;i<=arraysize-1;++i)
+	cout<<" \n While entering the elements, enter an element and press " Enter " key to type the next one ";
+	for(i=0;i<=arraysize-1;++i)			//taking values for the no. of elements
 	{
 		cin>>a[i];
 	}
-	for(i=0;i<=arraysize-1;++i)
+	for(i=0;i<=arraysize-1;++i)			//printing values entered by user
 	{
 		cout<<" \n Elements : "<<a[i];
 	}
-	for(i=0;i<=arraysize-1;++i)
+	for(i=0;i<=arraysize-1;++i)			//calculating the sum of given elements	
 	{
 		sum=sum+a[i];
 	}
 	cout<<" \n Sum of the elements : "<<sum;
 	div=arraysize;
-	mean=sum/div;
-	cout<<" \n Arithmetic Mean of the elements is : "<<mean<<"\n";
+	mean=sum/div;					//calculating the Arithmetic Mean for the given elements
+	cout<<" \n Arithmetic Mean of the elements is : "<<mean<<"\n";		//Printing AM
 	system("pause");
 	return 0;
 }
